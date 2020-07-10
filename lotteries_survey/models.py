28188028f0cfd3d3c6e27a59b02763472b03971c
PrachiHejib <p.hejib@uea.ac.uk>
2020-07-10 16:49:00 +0100

    incorporating payoffs

--- lotteries_survey/models.py
+++ lotteries_survey/models.py
@@ -21,11 +21,11 @@ class Constants(BaseConstants):
     name_in_url = 'lotteries_survey'
     players_per_group = None
     num_rounds = 2
+    payoff = currency_range(c(0), c(20), c(10))
 
 
 class Subsession(BaseSubsession):
     pass
-
 
 class Group(BaseGroup):
     @property
@@ -36,14 +36,20 @@ class Group(BaseGroup):
     def lottery_image_B(self):
         return f"lotteries_survey/lottery_q{self.round_number}.jpg"
 
-
-
 class Player(BasePlayer):
     lotterychoice = models.StringField()
 
     @property
     def dieroll(self):
         return random.randint(1,100)
+
+    @property
+    def payoff(self):
+        if self.lotterychoice == 'A' and self.dieroll in range(1,11):
+            return c(0)
+        else:
+            return c(20)
+
 
 
 
